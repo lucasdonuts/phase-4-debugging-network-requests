675407f52d5e0879e6e0eeffd8edff1d4e8d4ac4
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-30 11:14:52 -0500

    Code along

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,34 @@
 class MoviesController < ApplicationController
+  # wrap_parameters format: []
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
   
   def index
     movies = Movie.all
     render json: movies
   end
 
+  def create
+    movie = Movi.create!(movie_params)
+    render json: movie
+  end
+
+  private
+
+  def movie_params
+    params.permit(
+      :title,
+      :year,
+      :length,
+      :director,
+      :description,
+      :poster_url,
+      :category,
+      :discount,
+      :female_director
+    )
+  end
+
+  # def render_invalid_response(invalid)
+  #   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  # validates :title, presence: true
+  # validates :year, presence: true
+  # validates :length, presence: true
+  # validates :director, presence: true
+  # validates :description, presence: true
+  # validates :poster_url, presence: true
+  # validates :category, presence: true
+  # validates :discount, acceptance: true
+  # validates :female_director, acceptance: true
 end
